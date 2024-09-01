92a88a9296bf4f1e4f7cd05d0a5eaf632eea9075
secret <luxunisgod123@gmail.com>
2024-09-01 21:14:42 +0800

    Add files via upload

---
+++ 2,Compartment/salmon_PE.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# mamba activate RNA
+fq1=$1
+fq2=$2
+rep_folder=$3
+
+#如果是双端测序，就选用下列命令
+salmon quant --gcBias -l A -1 ${fq1} -2 ${fq2}  -i /mnt/disk4/haitao/bysj_seu/ref_genome/ucsc_hg19/hg19.salmon_sa_index -g /mnt/disk4/haitao/bysj_seu/ref_genome/ucsc_hg19/hg19.gtf -o ${rep_folder} -p 20
+
+#如果是单端测序，就选用下列命令：
+# salmon quant --gcBias -l A -r ${fq1}   -i /mnt/disk4/haitao/bysj_seu/ref_genome/ucsc_hg19/hg19.salmon_sa_index -g /mnt/disk4/haitao/bysj_seu/ref_genome/ucsc_hg19/hg19.gtf -o ${rep_folder} -p 15
