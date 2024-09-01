95e07335fde0cada07f893c25c9fde289d783ba4
secret <luxunisgod123@gmail.com>
2024-09-01 11:21:30 +0800

    Add files via upload

---
+++ 1,HiC-pro upstream processing/cooler_balance.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#mamba activate hicexplorer
+
+cooler balance -p 20 MB231.mcool::/resolutions/10000
+cooler balance -p 20 MB231.mcool::/resolutions/20000
+cooler balance -p 20 MB231.mcool::/resolutions/40000
+cooler balance -p 20 MB231.mcool::/resolutions/100000
+cooler balance -p 20 MB231.mcool::/resolutions/500000
+
+cooler balance -p 20 HMEC_rep2.mcool::/resolutions/10000
+cooler balance -p 20 HMEC_rep2.mcool::/resolutions/20000
+cooler balance -p 20 HMEC_rep2.mcool::/resolutions/40000
+cooler balance -p 20 HMEC_rep2.mcool::/resolutions/100000
+cooler balance -p 20 HMEC_rep2.mcool::/resolutions/500000
+
+cooler balance -p 20 HMEC_rep1.mcool::/resolutions/10000
+cooler balance -p 20 HMEC_rep1.mcool::/resolutions/20000
+cooler balance -p 20 HMEC_rep1.mcool::/resolutions/40000
+cooler balance -p 20 HMEC_rep1.mcool::/resolutions/100000
+cooler balance -p 20 HMEC_rep1.mcool::/resolutions/500000
+
+cooler balance -p 20 HCC70_rep2.mcool::/resolutions/10000
+cooler balance -p 20 HCC70_rep2.mcool::/resolutions/20000
+cooler balance -p 20 HCC70_rep2.mcool::/resolutions/40000
+cooler balance -p 20 HCC70_rep2.mcool::/resolutions/100000
+cooler balance -p 20 HCC70_rep2.mcool::/resolutions/500000
+
+cooler balance -p 20 HCC70_rep1.mcool::/resolutions/10000
+cooler balance -p 20 HCC70_rep1.mcool::/resolutions/20000
+cooler balance -p 20 HCC70_rep1.mcool::/resolutions/40000
+cooler balance -p 20 HCC70_rep1.mcool::/resolutions/100000
+cooler balance -p 20 HCC70_rep1.mcool::/resolutions/500000
+
+cooler balance -p 20 BT549_rep2.mcool::/resolutions/10000
+cooler balance -p 20 BT549_rep2.mcool::/resolutions/20000
+cooler balance -p 20 BT549_rep2.mcool::/resolutions/40000
+cooler balance -p 20 BT549_rep2.mcool::/resolutions/100000
+cooler balance -p 20 BT549_rep2.mcool::/resolutions/500000
+
+
+cooler balance -p 20 BT549_rep1.mcool::/resolutions/10000
+cooler balance -p 20 BT549_rep1.mcool::/resolutions/20000
+cooler balance -p 20 BT549_rep1.mcool::/resolutions/40000
+cooler balance -p 20 BT549_rep1.mcool::/resolutions/100000
+cooler balance -p 20 BT549_rep1.mcool::/resolutions/500000
+
